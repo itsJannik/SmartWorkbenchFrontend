e81477d8e2f9770bdadd3eab5ddbf2df2c79bac3
Jannik Lorenz <nanni.lorenz12@web.de>
2019-05-25 19:03:52 +0200

    basic css and lego example

--- webappp/src/components/main/Content.js
+++ webappp/src/components/main/Content.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ContentDescription from './ContentDescription';
+import legoMan_headAndBody from '../../data/legoMan/headAndBody.png';
 
 const ContentWrapper = styled.section`
     /* one grid-column */
@@ -11,16 +11,17 @@ const ContentTitle = styled.h1`
 const ContentImage = styled.img`
 `
 
+const ContentDescription = styled.p`
+`
+
 const Content = (props) => (
     <ContentWrapper>
         <ContentTitle>
-
+        Kopf und Körper zusammensetzen
         </ContentTitle>
-        <ContentImage>
-
-        </ContentImage>
+        <ContentImage src={legoMan_headAndBody} />
         <ContentDescription>
-
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
         </ContentDescription>
     </ContentWrapper>
 )
